d6cd0214d2372ebfb1f57c961c3f76dc3a365e97
Matthew Hope <chickazama@gmail.com>
2024-04-03 15:35:14 +0100

    Define URL and endpoint constants

---
+++ constants.go
@@ -0,0 +1,16 @@
+package myna
+
+import "fmt"
+
+const (
+	BASE_URL_PROD = "https://api.starlingbank.com/api/v2"
+	BASE_URL_SB   = "https://api-sandbox.starlingbank.com/api/v2"
+)
+
+const (
+	ACCOUNTS_ENDPOINT_PROD = "/accounts"
+)
+
+var (
+	ACCOUNTS_URL_PROD = fmt.Sprintf("%s%s", BASE_URL_PROD, ACCOUNTS_ENDPOINT_PROD)
+)
