5aaa9db8b0f5a94f7d80b0e170bc013650e4a8bf
Matthew Hope <chickazama@gmail.com>
2024-04-03 15:57:32 +0100

    Define AccountIdentifier struct

---
+++ AccountIdentifier.go
@@ -0,0 +1,7 @@
+package myna
+
+type AccountIdentifier struct {
+	IdentifierType    string `json:"identifierType"`
+	BankIdentifier    string `json:"bankIdentifier"`
+	AccountIdentifier string `json:"accountIdentifier"`
+}
